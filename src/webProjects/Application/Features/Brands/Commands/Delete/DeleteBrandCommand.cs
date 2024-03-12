26042a8ecc768058b209085a69aad62e83aaf709
Melih DİNÇER <m_dincer41@hotmail.com>
2024-03-12 16:50:23 +0300

    Brand Delete işlemleri için Command ve CommandHandler'ın oluşturulması.

---
+++ src/webProjects/Application/Features/Brands/Commands/Delete/DeleteBrandCommand.cs
@@ -0,0 +1,10 @@
+﻿using Application.Features.Brands.Dtos;
+using MediatR;
+
+namespace Application.Features.Brands.Commands.Delete
+{
+    public class DeleteBrandCommand : IRequest<DeletedBrandResponse>
+    {
+        public int Id { get; set; }
+    }
+}
