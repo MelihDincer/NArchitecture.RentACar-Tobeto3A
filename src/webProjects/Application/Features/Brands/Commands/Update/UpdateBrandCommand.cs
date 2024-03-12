ffcf2ded5bd2cffceffc2e9f961852c9310975a7
Melih DİNÇER <m_dincer41@hotmail.com>
2024-03-12 16:50:30 +0300

    Brand  Update işlemleri için Command ve CommandHandler'ın oluşturulması.

---
+++ src/webProjects/Application/Features/Brands/Commands/Update/UpdateBrandCommand.cs
@@ -0,0 +1,10 @@
+﻿using Application.Features.Brands.Dtos;
+using MediatR;
+
+namespace Application.Features.Brands.Commands.Update;
+
+public class UpdateBrandCommand : IRequest<UpdatedBrandResponse>
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
---
+++ src/webProjects/Application/Features/Brands/Commands/Update/UpdateBrandCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Application.Features.Brands.Dtos;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Brands.Commands.Update;
+
+public class UpdateBrandCommandHandler : IRequestHandler<UpdateBrandCommand, UpdatedBrandResponse>
+{
+    private readonly IBrandRepository _brandRepository;
+    private readonly IMapper _mapper;
+
+    public UpdateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper)
+    {
+        _brandRepository = brandRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<UpdatedBrandResponse> Handle(UpdateBrandCommand request, CancellationToken cancellationToken)
+    {
+        Brand? brand = await _brandRepository.GetAsync(x => x.Id == request.Id);
+        _mapper.Map(request, brand);
+        Brand updatedBrand = await _brandRepository.UpdateAsync(brand);
+        UpdatedBrandResponse response = _mapper.Map<UpdatedBrandResponse>(updatedBrand);
+        return response;
+    }
+}
