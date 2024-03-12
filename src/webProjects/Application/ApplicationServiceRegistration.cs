3820f8b460afe677faf4da1eb3363c633ccaed69
Melih DİNÇER <m_dincer41@hotmail.com>
2024-03-12 16:53:22 +0300

    ApplicationServiceRegistration oluşturuldu.

---
+++ src/webProjects/Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application;
+
+public static class ApplicationServiceRegistration
+{
+    public static IServiceCollection AddApplicationService(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        return services;
+    }
+}
