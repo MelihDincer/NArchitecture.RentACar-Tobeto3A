808a4a5e8a591d358648fbb08730160b549ac02b
Melih DİNÇER <m_dincer41@hotmail.com>
2024-03-12 16:49:45 +0300

    Nullable işlemleri.

--- src/corePackages/Core.Persistence/Repositories/BaseEntity.cs
+++ src/corePackages/Core.Persistence/Repositories/BaseEntity.cs
@@ -5,8 +5,8 @@ public class BaseEntity<TId>
     public TId Id { get; set; }
 
     public DateTime CreatedDate { get; set; }
-    public DateTime UpdatedDate { get; set; }
-    public DateTime DeletedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
+    public DateTime? DeletedDate { get; set; }
 
     public BaseEntity()
     {
