833f43146390ecbc423143a45af6f871fade1c7c
Melih DİNÇER <m_dincer41@hotmail.com>
2024-03-12 16:51:10 +0300

    Brands için mappingprofiles eklenmesi.

---
+++ src/webProjects/Application/Features/Brands/Profiles/MappingProfiles.cs
@@ -0,0 +1,23 @@
+﻿using Application.Features.Brands.Commands.Create;
+using Application.Features.Brands.Commands.Delete;
+using Application.Features.Brands.Commands.Update;
+using Application.Features.Brands.Dtos;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Features.Brands.Profiles;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Brand, CreateBrandCommand>().ReverseMap();
+        CreateMap<Brand, CreatedBrandResponse>().ReverseMap();
+
+        CreateMap<Brand, DeleteBrandCommand>().ReverseMap();
+        CreateMap<Brand, DeletedBrandResponse>().ReverseMap();
+
+        CreateMap<Brand, UpdateBrandCommand>().ReverseMap();
+        CreateMap<Brand, UpdatedBrandResponse>().ReverseMap();
+    }
+}
