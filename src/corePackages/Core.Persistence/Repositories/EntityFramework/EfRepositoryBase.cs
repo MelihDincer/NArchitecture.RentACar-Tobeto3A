71681c47eeeea753dcba46982401bfdf68e7bf2a
Melih DİNÇER <m_dincer41@hotmail.com>
2024-03-12 16:49:34 +0300

    Güncelleme işlemi esnasında UpdatedDate bilgisinin güncellenmesi.

--- src/corePackages/Core.Persistence/Repositories/EntityFramework/EfRepositoryBase.cs
+++ src/corePackages/Core.Persistence/Repositories/EntityFramework/EfRepositoryBase.cs
@@ -53,6 +53,7 @@ public class EfRepositoryBase<TEntity, TEntityId, TContext> : IAsyncRepository<T
 
     public async Task<TEntity> UpdateAsync(TEntity entity)
     {
+        entity.UpdatedDate = DateTime.UtcNow;
         Context.Update(entity);
         await Context.SaveChangesAsync();
         return entity;
