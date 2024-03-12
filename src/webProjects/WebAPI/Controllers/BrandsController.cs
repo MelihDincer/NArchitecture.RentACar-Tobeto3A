0f008b6eb9be130ca89091d58aa499a73aee3474
Melih DİNÇER <m_dincer41@hotmail.com>
2024-03-12 16:55:07 +0300

    BrandsController oluşturuldu.

---
+++ src/webProjects/WebAPI/Controllers/BrandsController.cs
@@ -0,0 +1,31 @@
+﻿using Application.Features.Brands.Commands.Create;
+using Application.Features.Brands.Commands.Delete;
+using Application.Features.Brands.Commands.Update;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : BaseController
+    {
+        [HttpPost("Add")]
+        public async Task<IActionResult> AddAsync([FromBody] CreateBrandCommand command)
+        {
+            return Created("", await Mediator.Send(command));
+        }
+
+        [HttpDelete("Delete")]
+        public async Task<IActionResult> DeleteAsync([FromBody] DeleteBrandCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+
+        [HttpPut("Update")]
+        public async Task<IActionResult> UpdateAsync([FromBody] UpdateBrandCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+    }
+}
