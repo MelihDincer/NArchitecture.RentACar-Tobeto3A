c62ee093154f86a3da9d196ad66b56490acb0f92
Melih DİNÇER <m_dincer41@hotmail.com>
2024-03-21 16:53:21 +0300

    Core.Security katmanı projeye eklendi.

---
+++ Core.Security/Entities/UserOperationClaim.cs
@@ -0,0 +1,23 @@
+﻿using Core.Persistence.Repositories;
+
+namespace Core.Security.Entities;
+
+public class UserOperationClaim : BaseEntity<int>
+{
+    public int UserId { get; set; }
+    public int OperationClaimId { get; set; }
+
+    public virtual User User { get; set; }
+    public virtual OperationClaim OperationClaim { get; set; }
+
+    public UserOperationClaim()
+    {
+    }
+
+    public UserOperationClaim(int id, int userId, int operationClaimId)
+    {
+        Id = id;
+        UserId = userId;
+        OperationClaimId = operationClaimId;
+    }
+}
